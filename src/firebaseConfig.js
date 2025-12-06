12d9b14ed906c587dc8ddfa2acd938cdff8e7aff
Peagasss <pedrohcsantos.pessoal@gmail.com>
2025-12-06 16:24:32 -0300

    Protegendo chaves de API

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,16 @@
-// Import the functions you need from the SDKs you need
+// src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Agora usamos import.meta.env para puxar as variáveis escondidas
 const firebaseConfig = {
-  apiKey: "AIzaSyBsnmsksDqHmQB-qjgkxXaGXO_cAMgcRSc",
-  authDomain: "meu-projeto-80ead.firebaseapp.com",
-  projectId: "meu-projeto-80ead",
-  storageBucket: "meu-projeto-80ead.firebasestorage.app",
-  messagingSenderId: "1002815722396",
-  appId: "1:1002815722396:web:e1ee2af0e304c660d0ae59"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
